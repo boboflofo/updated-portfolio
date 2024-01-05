784cbe5a754c8545f9be6770a68cd7ca5fecb991
Jonathan Song <jonats10@uci.edu>
2024-01-05 15:52:03 -0800

    link logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>My Portfolio</h1>
+      <h1>Jonathan Song</h1>
     </header>
   );
 };
--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -1,15 +1,15 @@
 const projectsData = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'A description of Project 1.',
-    imageUrl: 'https://via.placeholder.com/300',
+    title: '2048',
+    description: 'A remake of the game 2048 using HTML CSS and React',
+    imageUrl: 'https://github.com/boboflofo/2048.git',
   },
   {
     id: 2,
-    title: 'Project 2',
-    description: 'A description of Project 2.',
-    imageUrl: 'https://via.placeholder.com/300',
+    title: 'Doge Quiz',
+    description: 'A doge inspired trivia game utilizing HTML CSS and React',
+    imageUrl: 'https://github.com/boboflofo/DogeQuiz.git',
   },
   {
     id: 3,
