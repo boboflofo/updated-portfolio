bdfcd047aa0232246bcb86cdb9598ab1f07649b0
Jonathan Song <jonats10@uci.edu>
2024-01-05 16:30:53 -0800

    image logic

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProjectCard = ({ project }) => {
-  const { title, description, imageUrl } = project;
+  const { id, title, description, link, image } = project;
 
   return (
     <div className="project-card">
       <div className="card">
-        <img src={imageUrl} className="card-img-top" alt={title} />
+        
         <div className="card-body">
+        <img src='./assets/2048-pic.jpg'></img>
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
+          <a href={link} className="btn btn-primary">
+            View Project
+          </a>
         </div>
       </div>
     </div>
@@ -19,9 +23,10 @@ const ProjectCard = ({ project }) => {
 
 ProjectCard.propTypes = {
   project: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -3,19 +3,21 @@ const projectsData = [
     id: 1,
     title: '2048',
     description: 'A remake of the game 2048 using HTML CSS and React',
-    imageUrl: 'https://github.com/boboflofo/2048.git',
+    link: 'https://github.com/boboflofo/2048.git',
+    image: './../assets/2048-pic.jpg'
   },
   {
     id: 2,
     title: 'Doge Quiz',
     description: 'A doge inspired trivia game utilizing HTML CSS and React',
-    imageUrl: 'https://github.com/boboflofo/DogeQuiz.git',
+    link: 'https://github.com/boboflofo/DogeQuiz.git',
+    image: './../assets/doge-quiz-pic.jpg'
   },
   {
     id: 3,
     title: 'Project 3',
     description: 'A description of Project 3.',
-    imageUrl: 'https://via.placeholder.com/300',
+    link: 'https://via.placeholder.com/300',
   },
 ];
 
